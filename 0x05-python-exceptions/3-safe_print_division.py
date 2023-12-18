5857910c50d6fe8771b7c3e8e2123c849ef78838
leulmintesinot <leulminti@gmail.com>
2023-12-18 05:52:02 -0800

    3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(x, y):
+    try:
+        div = x / y
+    except (ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+        return div
